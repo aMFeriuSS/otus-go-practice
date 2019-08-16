abb426265518c383215ae406426d6f802f1532bb
Ilya Markin <amferiuss@gmail.com>
2019-08-16 14:37:05 +0300

    Shortener for main practice_test.go

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/amferiuss/otus-go-practice
 
 go 1.12
 
-require github.com/amferiuss/otus-go-practice/ntp v0.1.0
+require (
+	github.com/amferiuss/otus-go-practice/ntp v0.1.0
+	github.com/amferiuss/otus-go-practice/shortener v0.2.0
+)
--- practice_test.go
+++ practice_test.go
@@ -1,10 +1,25 @@
 package practice
 
-import "testing"
-import "github.com/amferiuss/otus-go-practice/ntp"
+import (
+	"testing"
+	"github.com/amferiuss/otus-go-practice/ntp"
+	"github.com/amferiuss/otus-go-practice/shortener"
+)
 
 // Week-1: ntp.
 func Test_NtpTime(t *testing.T) {
 	ntp.Time()
 	ntp.NtpTime()
+}
+
+func Test_Shortener(t *testing.T) {
+	shortnr := shortener.NewShortener()
+
+	link := "http://bing.com/search?q=golang"
+	want := "bing.com/0a17b5"
+
+	shortLink := shortnr.Shorten(link)
+	if shortLink != want {
+		t.Errorf("shortener.Shorten() = %q, want %q", shortLink, want)
+	}
 }
